7318e453e6427c5171ee4183c932f5f60b552c8a
Heandy27 <andy_heredia2008@hotmail.com>
2025-03-18 19:17:17 -0500

    Initial Commit

---
+++ Pokemon Project/Pokemon_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_ProjectApp.swift
+//  Pokemon Project
+//
+//  Created by Andy Heredia on 18/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Pokemon Project/Shared/Presentation/Home/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokemon Project
+//
+//  Created by Andy Heredia on 18/3/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
